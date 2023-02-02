0a5a6a322fabe3dfffd42bc358a86bb9197df754
conisonas <conisonas@outlook.com>
2023-02-02 19:54:45 +0800

    完成导航栏

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div  class="min-h-full font-Poppins box-border">
+    <Navigation />
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+  import Navigation from './components/Navigation.vue'
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="bg-at-light-green text-white">
+    <nav class="container py-5 px-4 flex flex-col gap-4 items-center sm:flex-row">
+      <div class="flex items-center gap-x-4">
+        <img class="w-14" src="../assets/images/dumbbell-light.png" alt="" />
+        <h1 class="text-lg">Sport</h1>
+      </div>
+      <ul class="flex flex-1 justify-end gap-x-10">
+        <router-link class="cursor-pointer" :to="{ name: '' }">首页</router-link>
+        <router-link class="cursor-pointer" :to="{ name: '' }">创建</router-link>
+        <router-link class="cursor-pointer" :to="{ name: '' }">登录</router-link>
+       
+      </ul>
+    </nav>
+  </header>
+</template>
